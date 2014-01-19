8aa6d506c10ee254c0255fd1fc0777d2b62d7323
teabait <hello.teaho@gmail.com>
2014-01-18 23:10:02 -0500

    add comments for extra clarity

--- tom2.js
+++ tom2.js
@@ -8,35 +8,46 @@
 var sentence = "Hello this is Tea. I like you! Maybe? You are smelly; all reindeer are smelly.";
 var sentencesWithCount = [];
 
+// splits input into array of separate sentences
 function splitSentences(input) {
+  // regular expression is period, exclamation points, and question marks with any character after them
+  // must include . after bracket or else one sentence returned will be an empty string
   var regEx = new RegExp("[\.\!\?].");
   return input.split(regEx);
 }
 
+// returns number of words in a sentence
 function countWords(string) {
   var wordsArray = string.split(" ");
   return wordsArray.length;
 }
 
+// print out all words in that sentence that are longer than 4 characters.
 function longWord(string) {
   var wordsArray = string.split(" ");
   for(i in wordsArray) {
+    // make sure word does not include punction
     var regEx = new RegExp("[\.\!\?\;]");
     var cleanedWord = wordsArray[i].replace(regEx, "");
+    // if word is longer than 4 characters, print that word
     if(cleanedWord.length > 4){
       console.log(cleanedWord);
     }
   }
 }
 
+// divides up input text into an array of sentences
 var sentencesArray = splitSentences(sentence);
 
+// counts number of words in each sentence within array of sentences and creates a hash object that contains the count
 for(i in sentencesArray) {
   var count = countWords(sentencesArray[i]);
   var sentenceWithCount = {count: count, sentence: sentencesArray[i]};
+  // push all of these objects in another array
   sentencesWithCount.push(sentenceWithCount);
 }
 
+//sort sentences according to number of words. Largest number of words is pushed to first in array.
 sentencesWithCount.sort(function (a, b) {
   if(a.count > b.count) {
     return -1;
@@ -47,6 +58,9 @@ sentencesWithCount.sort(function (a, b) {
   return 0;
 });
 
+// print out longest sentence with word count.
 console.log(sentencesWithCount[0].sentence, "has", sentencesWithCount[0].count, "words in it");
+
+// print out words within longest sentence that have more than 4 characters
 longWord(sentencesWithCount[0].sentence);
 
